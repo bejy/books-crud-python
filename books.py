6eba7682e0c7797fd950f118fa07bc8bc50ee39f
brianjamescabantac <noitshilarious@gmail.com>
2019-01-09 23:55:17 +0800

    Update books.py

--- books.py
+++ books.py
@@ -1,107 +1,106 @@
 import pymysql
-import sys
 
-connection = pymysql.connect(host = 'localhost',user = 'root',password = '', db = 'books')   
-    
-choices = 1
-while choices:
-    print ("[1] = Add new Book\n")
-    print ("[2] = Show lists of Books\n")
-    print ("[3] = Update Books\n")
-    print ("[4] = Delete Books\n")
-    print ("[5] = Exit\n")
+connection = pymysql.connect(host = 'localhost',user = 'root',password = '',db = 'books')
 
-    choice = input("Input task: ")
 
-    if choice == "1":
-         book_title = raw_input("Enter Book title: ")
-         book_author= raw_input("Enter Book Author: ")
-         book_rating= raw_input("Enter rating: ")
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO books ('book_title', 'book_author', 'book_rating') VALUES (%s, %s, %s)"
-            try:
-                cursor.execute(sql, (book_title, book_author, book_rating))
-                print("Added successfully")
-            except:
-                print("Something wrong")
-                
-        connection.commit()
-    finally:
-        print ("")
-  
-    
-    if choice == "2":
-        print ("Books\n")
-    try:
-        with connection.cursor() as cursor:
-            sql = "select * from books"
+def create(title,author,genre): 
+    with connection.cursor() as cursor:
+    	sql = 'INSERT INTO books (id ,title,author,genre) VALUES ( NULL, %s, %s, %s);'
+    	try:
+    		cursor.execute(sql, (title, author, genre))
+    		print('\nSuccessfully Added!')
+    	except:
+    		print('\nSomething went wrong!')
+    connection.commit()
+
+def read(create):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM books"
+        try:
             cursor.execute(sql)
-            connection.commit()
             results = cursor.fetchall()
-	    print '%10s'%("ID #         :"),
+	    print '%10s'%('\nID #       	:'),
 	    for row in results:
 		print '%10d'%(row[0]),
 	    print '\n'
-	    print '%10s'%("Book Title   :"),
+	    print '%10s'%('Book Title 	:'),
 	    for row in results:
 		print '%10s'%(row[1]),
 	    print '\n'
-	    print '%10s'%("Book Author  :"),
+	    print '%10s'%('Book Author	:'),
 	    for row in results:
 		print '%10s'%(row[2]),
 	    print '\n'
-	    print '%10s'%("Book rating  :"),
+	    print '%10s'%('Book Genre 	:'),
 	    for row in results:
 		print '%10s'%(row[3]),
-        connection.commit()
-    finally:
-        print ("")
-    
-    
-    
-    if choice == "3":
-    	print("Books\n")
-    	id = input("Enter your Book ID to Update: ")
-    	books_title = raw_input("Enter the new book title: ")
-    	book_author = raw_input("Enter the new book author: ")
-    	book_rating = raw_input("Enter the new book rating: ")
-    try:
-        with connection.cursor() as cursor:
-            sql = "UPDATE books SET 'book_title'=%s, 'book_author'=%s , 'book_rating'=%s WHERE 'id'= %s"
-            try:
-                cursor.execute(sql, (book_title, book_author, book_rating, id))
-                print("Successfully Updated")
-            except:
-                print("Something wrong")
- 
-        connection.commit()
-    finally:
-        print ("")
-      
-    
-    
-    if choice == "4":
-	print("Books\n")
-    	id = input("Enter the ID of the Book to delete: ")
-    try:
-        with connection.cursor() as cursor:
-            sql = "DELETE FROM books WHERE id = %s"
-            try:
-                cursor.execute(sql, (id))
-                print("Successfully Deleted")
-            except:
-                print("Something wrong")
- 
-        connection.commit()
-    finally:
-        print ("")
-       
-    
-    
-    if choice == "5":
-        sys.exit(0)
-        
+	except:
+	     print('\nSomething went wrong!')
+
+    connection.commit()
+
+
+def update(id):
+    with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM books WHERE  id = %s',id)
+            cursor.fetchall()
+            if cursor.rowcount > 0 :
+                new_title = raw_input('\nEnter New Title: ')
+                new_author = raw_input('Enter New Author: ')
+                new_genre = raw_input('Enter New Genre: ')
+                cursor.execute('UPDATE books SET title = %s, author = %s, genre=%s WHERE id = %s',(new_title,new_author,new_genre,id))
+                print('\nSuccessfully Updated!')
+            else:
+                print("\nDoesn't exist")
+
+    connection.commit()
+
+
+def delete(id):
+    with connection.cursor() as cursor:
+        sql = 'SELECT * FROM books WHERE id = %s'
+        try:
+            cursor.execute(sql, id)
+            if cursor.rowcount > 0 :
+                delete = "DELETE FROM books WHERE id = " + "'" + id + "'"
+                cursor.execute(delete)
+                print ('\nSuccessfully Deleted!')
+            else:
+                print "\nDoesn't Exist!"
+        except:
+            print ('\nSomething went wrong!')
+
+    connection.commit()
+
+""" Choices """
+x = 1
+while x:
+    print '\n'
+    print '[C] Create'
+    print '[R] Read'
+    print '[U] Update'
+    print '[D] Delete'
+    print '[E] Exit\n'
+
+
+
+    choice = raw_input('Enter Choice: ')
+
+    if choice == 'c' or choice == 'C':
+        title  = raw_input('\nBook Title   : ')
+        author  = raw_input('Book Author  : ')
+        genre  = raw_input('Book Genre   : ')
+        create(title,author,genre)
+    elif choice == 'r' or choice == 'R':
+        read(create)
+    elif choice == 'u' or choice == 'U':
+        id = raw_input('\nEnter ID that you wish to Update:')
+        update(id)
+    elif choice == 'd' or choice == 'D':
+        id = raw_input('\nEnter ID that you wish to delete: ')
+        delete(id)
+    elif choice == 'e' or choice == 'E':   
+
+        break 
     else:
-        print ("Invalid Input!\n")
-        choices = 1
+ 	print "Invalid Choice"
